5666c5a15f75f923b8a929580460b7e5472a7c29
GTK-kala <gtkkala1110@gmail.com>
2025-03-23 11:21:33 +0300

    second

--- To-do-list-React-app/src/App.jsx
+++ To-do-list-React-app/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <h1 className="text-gray-800 font-bold text-2xl uppercase">To-Do List</h1>
     </div>
     <form className="w-full max-w-sm mx-auto px-4 py-2">
-        <div className="flex items-center border-b-2 border-teal-500 py-2">
+        <div className="flex items-center border-b-2 border-teal-500 py-2">T
             <input
                 className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
                 type="text" placeholder="Add a task" />
@@ -23,7 +23,7 @@ function App() {
         <li className="py-4">
             <div className="flex items-center">
                 <input id="todo1" name="todo1" type="checkbox"
-                    className="h-4 w-4 text-teal-600 focus:ring-teal-500 border-gray-300 rounded" />
+                    className="h-4 w-4 text-teal-600 focus:ring-teal-500 border-gray-300 rounded hover:cursor-pointer" />
                 <label for="todo1" class="ml-3 block text-gray-900">
                     <span className="text-lg font-medium">Finish project proposal</span>
                     <span className="text-sm font-light text-gray-500">Due on 4/1/23</span>
@@ -33,7 +33,7 @@ function App() {
         <li className="py-4">
             <div className="flex items-center">
                 <input id="todo2" name="todo2" type="checkbox"
-                    className="h-4 w-4 text-teal-600 focus:ring-teal-500 border-gray-300 rounded" />
+                    className="h-4 w-4 text-teal-600 focus:ring-teal-500 border-gray-300 rounded hover:cursor-pointer" />
                 <label for="todo2" class="ml-3 block text-gray-900">
                     <span className="text-lg font-medium">Buy groceries</span>
                     <span className="text-sm font-light text-gray-500">Bananas, milk, bread</span>
@@ -43,7 +43,7 @@ function App() {
         <li className="py-4">
             <div className="flex items-center">
                 <input id="todo3" name="todo3" type="checkbox"
-                    className="h-4 w-4 text-teal-600 focus:ring-teal-500 border-gray-300 rounded" />
+                    className="h-4 w-4 text-teal-600 focus:ring-teal-500 border-gray-300 rounded hover:cursor-pointer" />
                 <label for="todo3" className="ml-3 block text-gray-900">
                     <span className="text-lg font-medium">Go for a run</span>
                     <span className="text-sm font-light text-gray-500">3 miles</span>
